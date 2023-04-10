6705a11b0dfd1138494a8760cad29fd3cc684a81
Đức <64053044+steveho29@users.noreply.github.com>
2023-04-10 19:47:04 +0700

    impl: voice & language & save/delete messages

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import "dart:convert";
+import 'package:path/path.dart';
 import "dart:io";
+import 'package:sembast/sembast.dart';
+import 'package:sembast/sembast_io.dart';
 
 // import "package:file_picker/file_picker.dart";
+import "package:flag/flag.dart";
 import "package:flutter/material.dart";
 import "package:flutter/services.dart" show rootBundle;
 import "package:flutter_chat_types/flutter_chat_types.dart" as types;
@@ -9,11 +13,8 @@ import "package:flutter_chat_ui/flutter_chat_ui.dart";
 import "package:flutter_dotenv/flutter_dotenv.dart";
 import "package:get_it/get_it.dart";
 import "package:http/http.dart" as http;
-// import "package:image_picker/image_picker.dart";
 import "package:intl/date_symbol_data_local.dart";
-// import "package:mime/mime.dart";
-// import "package:open_filex/open_filex.dart";
-// import "package:path_provider/path_provider.dart";
+import "package:path_provider/path_provider.dart";
 import "package:uuid/uuid.dart";
 
 Future main() async {
@@ -82,17 +83,47 @@ class _ChatPageState extends State<ChatPage> {
     _loadMessages();
   }
 
+  ChatTheme theme = const DarkChatTheme();
+  bool isVn = false;
   @override
   Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          backgroundColor: theme.inputBackgroundColor,
+          leading: IconButton(
+            icon: Icon(Icons.delete_forever_rounded,
+                color: theme.errorColor, size: 40),
+            onPressed: _clearMessages,
+          ),
+          actions: [
+            GestureDetector(
+                onTap: () {
+                  setState(() {
+                    isVn = !isVn;
+                  });
+                  // chatController.addChat();
+                },
+                child: Flag.fromString(
+                  isVn ? 'vn' : 'us',
+                  width: 50,
+                )),
+            const SizedBox(width: 16),
+          ],
+          centerTitle: true,
+          titleSpacing: 0,
+        ),
         body: Chat(
           messages: _messages,
-          onAttachmentPressed: _handleAttachmentPressed,
+          // onAttachmentPressed: _handleAttachmentPressed,
+          inputOptions: InputOptions(
+              sendButtonVisibilityMode: SendButtonVisibilityMode.always),
+          theme: theme,
           onMessageTap: _handleMessageTap,
           onPreviewDataFetched: _handlePreviewDataFetched,
           onSendPressed: _handleSendPressed,
           showUserAvatars: true,
           showUserNames: true,
           user: _user,
+          l10n: isVn ? const ChatL10nVi() : const ChatL10nEn(),
           typingIndicatorOptions: TypingIndicatorOptions(
               typingUsers: isWaitingForGPT ? [_gpt] : []),
         ),
@@ -104,47 +135,10 @@ class _ChatPageState extends State<ChatPage> {
     });
   }
 
-  void _handleAttachmentPressed() {
-    showModalBottomSheet<void>(
-      context: context,
-      builder: (BuildContext context) => SafeArea(
-        child: SizedBox(
-          height: 144,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                  // _handleImageSelection();
-                },
-                child: const Align(
-                  alignment: AlignmentDirectional.centerStart,
-                  child: Text("Photo"),
-                ),
-              ),
-              TextButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                  // _handleFileSelection();
-                },
-                child: const Align(
-                  alignment: AlignmentDirectional.centerStart,
-                  child: Text("File"),
-                ),
-              ),
-              TextButton(
-                onPressed: () => Navigator.pop(context),
-                child: const Align(
-                  alignment: AlignmentDirectional.centerStart,
-                  child: Text("Cancel"),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
+  void _addMessageReversed(types.Message message) {
+    setState(() {
+      _messages.insert(_messages.length, message);
+    });
   }
 
   void _handleMessageTap(BuildContext _, types.Message message) async {
@@ -206,7 +200,7 @@ class _ChatPageState extends State<ChatPage> {
     });
   }
 
-  void _handleSendPressed(types.PartialText message) async {
+  _handleSendPressed(types.PartialText message) async {
     final textMessage = types.TextMessage(
       author: _user,
       createdAt: DateTime.now().millisecondsSinceEpoch,
@@ -232,16 +226,39 @@ class _ChatPageState extends State<ChatPage> {
     });
 
     _addMessage(answerMessage);
+    _saveMessages();
+  }
+
+  void _saveMessages() async {
+    var dir = await getApplicationDocumentsDirectory();
+    await dir.create(recursive: true);
+    var dbPath = join(dir.path, 'chat.db');
+    var db = await databaseFactoryIo.openDatabase(dbPath);
+    var store = StoreRef.main();
+    String jsonString = jsonEncode(_messages);
+    await store.record('chat').put(db, jsonString);
+    await db.close();
   }
 
   void _loadMessages() async {
-    // final response = await rootBundle.loadString("assets/messages.json");
-    // final messages = (jsonDecode(response) as List)
-    //     .map((e) => types.Message.fromJson(e as Map<String, dynamic>))
-    //     .toList();
+    var dir = await getApplicationDocumentsDirectory();
+    await dir.create(recursive: true);
+    var dbPath = join(dir.path, 'chat.db');
+    var db = await databaseFactoryIo.openDatabase(dbPath);
+    var store = StoreRef.main();
+    var chat = await store.record('chat').get(db);
+    var messages = jsonDecode(chat as String);
+    for (var msg in messages) {
+      _addMessageReversed(types.TextMessage.fromJson(msg));
+    }
+    await db.close();
+  }
 
-    // setState(() {
-    //   _messages = messages;
-    // });
+  void _clearMessages() {
+    setState(() {
+      _messages.removeWhere((element) => true);
+    });
+
+    _saveMessages();
   }
 }
